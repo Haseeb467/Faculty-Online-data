b4464130c745eecd5e6556218a4724913c6ef619
Haseeb467 <155372673+Haseeb467@users.noreply.github.com>
2025-01-27 09:48:07 +0500

    Update dbconfig.ts

--- src/firebasedbconfig/dbconfig.ts
+++ src/firebasedbconfig/dbconfig.ts
@@ -4,7 +4,14 @@ import { getStorage } from "firebase/storage";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  
+    apiKey: "AIzaSyD7sC13osQQuH8FW6NbAOxiqcHPACF2Vng",
+  authDomain: "fms-4-ts.firebaseapp.com",
+  databaseURL: "https://fms-4-ts-default-rtdb.firebaseio.com",
+  projectId: "fms-4-ts",
+  storageBucket: "fms-4-ts.firebasestorage.app",
+  messagingSenderId: "776841904816",
+  appId: "1:776841904816:web:9f7a8edcb1471ac64eb175",
+  measurementId: "G-92RN4N49NX"
 };
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
